e446ba678278350e176b574ab88778bd7f3056c2
Waldemar Hummer <waldemar.hummer@gmail.com>
2023-11-09 17:24:09 +0100

    add IAM policies to TF script

--- lambdas/handler.js
+++ lambdas/handler.js
@@ -4,14 +4,17 @@ const { SQS } = require('@aws-sdk/client-sqs');
 exports.handler = async (event) => {
   console.log(JSON.stringify(event));
 
+  // scan items from DynamoDB table
   const ddb = new DynamoDB();
   const result = await ddb.scan({TableName: "test"});
   console.log("DynamoDB result", result);
 
+  // send a message to the SQS queue
   const sqs = new SQS();
-  const params = {QueueUrl: "http://localhost:4566/000000000000/test", MessageBody: "test 123"};
+  const message = "Hello from LocalStack!";
+  const params = {QueueUrl: "http://localhost:4566/000000000000/test", MessageBody: message};
   const result2 = await sqs.sendMessage(params);
   console.log("SQS result", result2);
 
-  return "Hello from LocalStack!";
+  return {result: message};
 }
